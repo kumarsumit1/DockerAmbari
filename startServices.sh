ba2c687e66abe1ad64920c124a1e545d6ced21d3
Sumit Kumar <ursumit@gmail.com>
2018-05-28 00:15:34 +0530

    added verbose command

--- startServices.sh
+++ startServices.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#start Ambari Server
+CLUSTER='hdp-ambari'
+HOST='node.monocluster.com'
+AMBARI_HOST=$HOST
+USER='admin'
+PASSWORD='admin'
+
+
 #start Services
 
 service ntp start
@@ -12,7 +20,14 @@ ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql.jar
 mysql -uroot -pkylo -e 'CREATE USER `hive`@`localhost` IDENTIFIED BY `kylo`; GRANT ALL PRIVILEGES ON *.* TO `hive`@`localhost`; CREATE USER `hive`@`%` IDENTIFIED BY `kylo`;GRANT ALL PRIVILEGES ON *.* TO `hive`@`%`; CREATE USER `hive`@`node.monocluster.com` IDENTIFIED BY `kylo`; GRANT ALL PRIVILEGES ON *.* TO `hive`@`node.monocluster.com`; FLUSH PRIVILEGES; CREATE DATABASE hive;
 '
 
-#start Ambari Server
+
+
+#while [ -z "$(ss -ltunp | grep 8080)" ]; do
+#  ambari-server start -v
+  #ambari-agent start
+#  sleep 20
+#done
+
 ambari-server start -v
 
 #If no postgresql is not initialized
